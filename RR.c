b4650c29c9544a4d6eb8f9a4662c542224dd19b9
KnowHow866 <accidental001@gmail.com>
2017-04-23 01:41:04 +0800

    FISO

--- RR.c
+++ RR.c
@@ -22,12 +22,13 @@ void GetTime(pid_t pid)
     }
 }
 
-struct Queue_T
+struct _Queue_T
 {
       char pname[POLICY_LEN];
       int ready_time;
       int exec_time;
-}
+};
+typedef struct _Queue_T Queue_T;
 
 struct remain{
 	pid_t pid;
@@ -35,22 +36,49 @@ struct remain{
 	int nice;
 } remain_P[POLICY_LEN];
 
-struct input process[MAX_QUEUE_LEN];
+void RR(Queue_T* P_Queue, int Process_Num);
 
 char Policy[POLICY_LEN];
 int Process_Num = 0;
 
 int main(){
+
 	scanf("%s",&Policy);
 	scanf("%d",&Process_Num);
-	Queue_T *P_Queue = (*Queue_T)malloc((Queue_T)*Process_Num);
+	Queue_T* P_Queue = (Queue_T*)malloc(sizeof(Queue_T)*Process_Num);
 	for(int i = 0; i < Process_Num; i++){
-		scanf(" %s %d %d",&P_Queue[i]->pname, &P_Queue[i]->ready_time, &P_Queue[i]->exec_time);
+		scanf(" %s %d %d",
+			&(P_Queue[i].pname), 
+			&(P_Queue[i].ready_time), 
+			&(P_Queue[i].exec_time));
 	}  
 
-	for(int i = 0;i < Process_Num; i++){
-		printf("%s\n",P_Queue[i]->pname);
-		printf("%d %d",P_Queue[i]->ready_time,P_Queue[i]->exec_time);
+	RR(P_Queue, Process_Num);
+}
+
+void RR(Queue_T* P_Queue, int Process_Num){
+	int quantum = 500;
+	printf("Start: ");
+	GetTime(getpid());
+
+	pid_t pid;
+	for(int i = 0; i < Process_Num; i++){
+		pid = fork();
+
+		if(pid < 0){
+	 		puts("Error pid");
+		}
+		else if(pid == 0){
+			volatile unsigned long i;
+			for(i = 0; i < 1000000UL* P_Queue[i].ready_time ;i++);
+			GetTime(getpid());
+			//exit();
+		}
+		if(pid > 0){
+			wait(NULL);
+			GetTime(getpid());
+		}
 	}
+
 }
 
